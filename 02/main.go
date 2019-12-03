8d04dbcd00db39a19b714a569eb2c3ffe2762bc6
spaghettiwews <spaghettiwews@outlook.com>
2019-12-03 17:00:21 +0200

    day 2 solution - part 1

---
+++ 02/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"strconv"
+)
+
+func main() {
+	var strCodes []string
+	
+	input, err := os.Open("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer input.Close()
+	scanner := bufio.NewScanner(input)
+
+	for scanner.Scan() {
+		strCodes = strings.Split(scanner.Text(), ",")
+	}
+	
+	intCodes := make([]int, len(strCodes))
+
+	for i := 0; i < len(strCodes); i++ {
+		intCodes[i], _ = strconv.Atoi(strCodes[i])
+	}
+
+	for i := 0; i < len(intCodes) ; i+=4 {
+		if intCodes[i] == 1 {
+			intCodes[intCodes[i+3]] = intCodes[intCodes[i+1]] + intCodes[intCodes[i+2]]
+		}
+		if intCodes[i] == 2 {
+			intCodes[intCodes[i+3]] = intCodes[intCodes[i+1]] * intCodes[intCodes[i+2]]
+		}
+		if intCodes[i] == 99 {
+			break
+		}
+	}
+	fmt.Println(intCodes)
+}
+
